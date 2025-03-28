4fb70dfd7b43218f15feb2ad937e98e863858abd
Aayush Kumar <aayush98.ak@gmail.com>
2025-03-29 02:46:40 +0530

    Theme switcher

---
+++ 18themeswitcher/src/components/ThemeBtn.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import useTheme from '../contexts/Theme';
+
+export default function ThemeBtn() {
+    const {themeMode, lightMode, darkMode} = useTheme();
+   
+    const onChangeBtn = (e) => {
+        const darkModeStatus = e.currentTarget.checked;
+        if(darkModeStatus) {
+            darkMode();
+        } else {
+            lightMode();
+        }
+    }
+
+    return (
+        <label className="relative inline-flex items-center cursor-pointer">
+            <input
+                type="checkbox"
+                className="sr-only peer"
+                onChange={onChangeBtn}
+                checked={themeMode === 'dark'}
+            />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span style={{color: '#ffffff'}} className="ml-3 text-sm font-medium">
+                {themeMode === 'dark' ? 'Light Mode' : 'Dark Mode'}
+            </span>
+        </label>
+    );
+}
